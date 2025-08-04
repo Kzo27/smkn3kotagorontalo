5af6d3fe244c8084feb82c84016fa927dffd1e37
Kzo27 <145586826+Kzo27@users.noreply.github.com>
2025-08-04 13:39:41 +0800

    Update auth.php

--- admin/auth.php
+++ admin/auth.php
@@ -1,1 +1,11 @@
+<?php
+// Memulai atau melanjutkan sesi yang sudah ada
+session_start();
 
+// Cek apakah variabel sesi 'admin_logged_in' tidak ada atau tidak bernilai true
+if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
+    // Jika tidak, arahkan pengguna ke halaman login
+    header("location: login.php");
+    exit; // Pastikan untuk menghentikan eksekusi script setelah redirect
+}
+?>
